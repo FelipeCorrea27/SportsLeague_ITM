61f98e75adfbe8989a345417cbe87bcae3c4d9ef
Felipe <Felipe@DESKTOP-I5KBKKU>
2026-03-30 14:01:30 -0500

    Add Entity Sponsor and Configure DbContext with unique index on Name

--- SportsLeague.Domain/Entities/Player.cs
+++ SportsLeague.Domain/Entities/Player.cs
@@ -10,6 +10,7 @@ public class Player : AuditBase
 
     public string FirstName { get; set; } = string.Empty;
 
+
     public string LastName { get; set; } = string.Empty;
 
     public DateTime BirthDate { get; set; }
